e9f0dd90fb3992b86f0ea34f18cb1d337609e9bb
Lucas Avila <lukas.avila02@gmail.com>
2023-09-11 18:33:08 -0300

    primeiro commit

---
+++ FakePinterest/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SECRET_KEY"] = "745efd8ef58f83eefa1c33c173b65f00"
+app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
+
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_maneger = LoginManager(app)
+login_maneger.login_view = "homepage"
+
+
+
+from FakePinterest import routes
